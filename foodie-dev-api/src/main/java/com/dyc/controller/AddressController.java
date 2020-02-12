4ed5b80ca91e9458b7ac51f00ac6d54a77586d00
dengyichao <919325340@qq.com>
2020-02-12 20:01:48 +0800

    商品订单收货地址模块完成

--- foodie-dev-api/src/main/java/com/dyc/controller/AddressController.java
+++ foodie-dev-api/src/main/java/com/dyc/controller/AddressController.java
@@ -26,14 +26,6 @@ public class AddressController {
 
     final static Logger logger = LoggerFactory.getLogger(AddressController.class);
 
-    /**
-     * 用户在确认订单页面，可以针对收获地址做如下操作：
-     * 1. 查询用户的所有收货地址列表
-     * 2. 新增收货地址
-     * 3. 删除收货地址
-     * 4. 修改收货地址
-     * 5. 设置默认地址
-     */
     @Autowired
     private AddressService addressService;
 
